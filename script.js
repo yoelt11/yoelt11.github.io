db184b3d2efec380e0d3b3bf75635c002df989a2
Edgar Torres <edgar-yoel.torres-rios@ipvs.uni-stuttgart.de>
2024-05-30 21:06:56 +0200

    [Feature] Added blog section

--- script.js
+++ script.js
@@ -1,67 +1,75 @@
 // script.js
-document.addEventListener('DOMContentLoaded', function () {
-    const articlesButton = document.querySelector('.toggle-articles');
-    const projectsButton = document.querySelector('.toggle-projects');
-    const articlesSection = document.querySelector('.articles');
-    const projectsSection = document.querySelector('.projects');
-    const articlesContainer = document.querySelector('.article-container');
-    const projectsContainer = document.querySelector('.project-container');
+document.addEventListener("DOMContentLoaded", function () {
+  const articlesButton = document.querySelector(".toggle-articles");
+  const blogsButton = document.querySelector(".toggle-blogs");
+  const projectsButton = document.querySelector(".toggle-projects");
+  const articlesContainer = document.querySelector(".article-container");
+  const blogsContainer = document.querySelector(".blogs-container");
+  const projectsContainer = document.querySelector(".project-container");
 
-    // Populate with articles and projects
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            populateSection(articlesContainer, data.articles, 'article');
-            populateSection(projectsContainer, data.projects, 'project');
-        })
-        .catch(error => console.error('Error fetching data:', error));
+  // Populate with articles and projects
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      populateSection(articlesContainer, data.articles, "article");
+      populateSection(blogsContainer, data.blogs, "blogs");
+      populateSection(projectsContainer, data.projects, "project");
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 
-    function populateSection(container, items, className) {
-        items.forEach((item, index) => {
-            const sectionDiv = document.createElement('div');
-            sectionDiv.classList.add(className);
-            sectionDiv.setAttribute('data-state', index < 2 ? 'show' : 'hide'); // Set initial data-state
-            sectionDiv.innerHTML = `
+  function populateSection(container, items, className) {
+    items.forEach((item, index) => {
+      const sectionDiv = document.createElement("div");
+      sectionDiv.classList.add(className);
+      sectionDiv.setAttribute("data-state", index < 2 ? "show" : "hide"); // Set initial data-state
+      sectionDiv.innerHTML = `
                 <h3>${item.title}</h3>
                 <p class="${className}-info">${item.info}</p>
             `;
-            container.appendChild(sectionDiv);
-        });
-    }
+      container.appendChild(sectionDiv);
+    });
+  }
 
-    // button function
-    function toggleSection(button, container) {
-        // Toggle visibility of hidden items on button click
-        const hiddenItems = container.querySelectorAll(`.${container.classList.contains('article-container') ? 'article' : 'project'}[data-state="hide"]`);
-        const showItems = container.querySelectorAll(`.${container.classList.contains('article-container') ? 'article' : 'project'}[data-state="show"]`);
-        const isExpanded = button.getAttribute('aria-expanded') === 'true';
+  // Generalized button function
+  function toggleSection(button, container, className) {
+    // Toggle visibility of hidden items on button click
+    const hiddenItems = container.querySelectorAll(
+      `.${className}[data-state="hide"]`,
+    );
+    const showItems = container.querySelectorAll(
+      `.${className}[data-state="show"]`,
+    );
+    const isExpanded = button.getAttribute("aria-expanded") === "true";
 
-        if (!isExpanded) {
-            hiddenItems.forEach(item => {
-                item.setAttribute('data-state', 'show');
-            });
-        } else {
-            const default_show = 2;
-            var i = 0;
-            showItems.forEach(item => {
-                if (i >= default_show) {
-                    item.setAttribute('data-state', 'hide');
-                }
-                i++;
-            });
+    if (!isExpanded) {
+      hiddenItems.forEach((item) => {
+        item.setAttribute("data-state", "show");
+      });
+    } else {
+      const default_show = 2;
+      var i = 0;
+      showItems.forEach((item) => {
+        if (i >= default_show) {
+          item.setAttribute("data-state", "hide");
         }
-
-        // Toggle the button text and aria-expanded attribute
-        button.textContent = !isExpanded ? '-' : '+';
-        button.setAttribute('aria-expanded', !isExpanded);
+        i++;
+      });
     }
 
-    articlesButton.addEventListener('click', function () {
-        toggleSection(articlesButton, articlesContainer);
-    });
+    // Toggle the button text and aria-expanded attribute
+    button.textContent = !isExpanded ? "-" : "+";
+    button.setAttribute("aria-expanded", !isExpanded);
+  }
 
-    projectsButton.addEventListener('click', function () {
-        toggleSection(projectsButton, projectsContainer);
-    });
+  articlesButton.addEventListener("click", function () {
+    toggleSection(articlesButton, articlesContainer);
+  });
+
+  projectsButton.addEventListener("click", function () {
+    toggleSection(projectsButton, projectsContainer);
+  });
+
+  blogsButton.addEventListener("click", function () {
+    toggleSection(blogsButton, blogsContainer);
+  });
 });
-
